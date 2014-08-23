7a1bcbd00773b92341a853308ec536dd41f23971
Ash Hoover <strixcuriosus@gmail.com>
2014-08-22 23:38:36 -0700

    Start tinkering

---
+++ tinker.hs
@@ -0,0 +1,11 @@
+square x = x * x
+atleast y thresh = if y > thresh
+                    then y
+                    else 0
+
+stuck = "inert by definition"
+cat = "fe" ++ "line"
+catsound = ['m', 'e'] ++ ['o', 'w']
+
+push item stack = item : stack
+pop stack = tail stack
